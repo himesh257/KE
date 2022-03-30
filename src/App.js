2f05a5402d24f189f95c1d49955a53e9cd88e870
Himesh Buch <ash@Himeshs-MBP.lan>
2022-03-30 19:31:42 -0400

    added basic navbar and dynamic cards

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./styles.css";
+import Card from "./Card";
+import NavBar from "./NavBar";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  gridContainer: {
+    paddingLeft: "40px",
+    paddingRight: "40px",
+    paddingTop: "40px"
+  }
+});
+
+const numCards = 10
+
+export default function App() {
+  const classes = useStyles();
+  return (
+    <><NavBar></NavBar>
+    <Grid container spacing={4} className={classes.gridContainer} justify="center">
+     {[...Array(numCards)].map((e, i) => {
+        return (
+          <Grid item xs={12} sm={6} md={4}>
+            <Card />
+          </Grid>
+        )
+      })
+    }
+    </Grid></>
+  );
+}
